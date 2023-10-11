e34f5982faf86ca79c65c1642de4c516cd1ad9a7
Nashid Shah <nashid.ahmed.115@gmail.com>
2023-10-11 02:17:44 -0400

    Added walletconnect

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center px-12 py-4">
+      <span className="font-mono text-2xl">Docissue</span>
+      <w3m-button />
+    </header>
+  )
+}
